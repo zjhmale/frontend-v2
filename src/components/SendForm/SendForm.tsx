309d17424914ed69c7e98e71e6e6167bd5ada196
Gamaranto <francesco@umaproject.org>
2022-03-18 10:52:07 +0100

    fix(across-v2): code review fixes
    Signed-off-by: Gamaranto <francesco@umaproject.org>

--- src/components/SendForm/SendForm.tsx
+++ src/components/SendForm/SendForm.tsx
@@ -10,26 +10,17 @@ import { SendFormProvider } from "hooks";
 import type { Deposit } from "views/Confirmation";
 
 type Props = {
-<<<<<<< HEAD
   onDepositConfirmed: (deposit: Deposit) => void;
 };
+
 const SendForm: React.FC<Props> = ({ onDepositConfirmed }) => {
-=======
-  onDeposit: (deposit: Deposit) => void;
-};
-const SendForm: React.FC<Props> = ({ onDeposit }) => {
->>>>>>> 2fd3351 (fix(across-v2): refactor confirmation screen)
   return (
     <SendFormProvider>
       <Layout>
         <ChainSelection />
         <CoinSelection />
         <AddressSelection />
-<<<<<<< HEAD
         <SendAction onDeposit={onDepositConfirmed} />
-=======
-        <SendAction onDeposit={onDeposit} />
->>>>>>> 2fd3351 (fix(across-v2): refactor confirmation screen)
       </Layout>
     </SendFormProvider>
   );
