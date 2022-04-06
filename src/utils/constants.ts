b21b8f342a1dbb6c587ec443a55904e93d112960
David <david@umaproject.org>
2022-04-06 11:34:16 -0400

    epic: release branch for qa
    Signed-off-by: David <david@umaproject.org>

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -567,6 +567,7 @@ export function makePoolClientConfig(chainId: ChainId): acrossSdk.pool.Config {
     rateModelStoreAddress,
   };
 }
+
 // default to kovan when testing
 // FIXME: Switch to Mainnet in prod, when we have a mainnet hub pool
 export const HUBPOOL_CHAINID = isProduction() ? ChainId.KOVAN : ChainId.KOVAN;
